a3f384ef85380ae4ad22f7de9de2bc8de72457f5
Sam Osman <sam.osman97@gmail.com>
2018-11-01 16:32:54 -0600

    Created a contact object and an array of Contacts

---
+++ Contact.js
@@ -0,0 +1,12 @@
+export class Contact{
+fName : "";
+lName : "";
+messengerID : "";
+phoneNumber : "";
+  constructor(fName, lName, messengerID, phoneNumber){
+  this.fName = fName;
+  this.lName = lName;
+  this.messengerID = messengerID;
+  this.phoneNumber = phoneNumber;
+  }
+  }
--- ContactList.js
+++ ContactList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
-
+import {Contact} from './Contact';
 export class ContactList extends React.Component {
   render() {
+	  var contactList = new Array();
+	  var contact = new Contact("Sam", "Osman", "12345", "123456");
+	  contactList.push(contact);
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Contact List Screen</Text>
+		<Text>{contactList[0].fName}</Text>
       </View>
     );
   }
